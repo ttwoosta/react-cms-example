065aa1938f5e472c638c01ea47068f7cb01c0b41
tu.tong <Tu.Tong@nirvanaHealth.com>
2023-06-30 16:38:45 -0400

    original files

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+// import '@cmsgov/design-system/dist/css/index.css';
+// import '@cmsgov/design-system/dist/css/core-theme.css';
+import './styles/index.scss';
+import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
